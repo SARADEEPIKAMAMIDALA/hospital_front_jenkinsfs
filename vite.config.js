a692a628bf9f5a7ebb247565fe4b6118063a4535
saradeepika03@gmail.com <saradeepika03@gmail.com>
2025-10-24 19:21:25 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hospital_frontend/', // 👈 VERY IMPORTANT: match your Tomcat context path
+  build: {
+    outDir: 'dist'
+  }
 })
